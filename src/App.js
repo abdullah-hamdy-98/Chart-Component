cb9b69bb29edc5edaf2964b35f63bb719210e5d1
abdullah-hamdy-98 <abdo1231992260@gmail.com>
2024-05-29 12:02:43 +0300

    second

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import './App.css';
+import HighChart3DComponent from './component/chartComponent';
+
+const data = await fetch(
+  'http://localhost:6060/categ'
+).then(response => response.json());
+
+
+const chartData = Object.entries(data).map(([Categ, Emp]) => ({
+  name: Emp.Categ,
+  y: Emp.Emp
+}));
+
+// Chart Types
+let chartType = {
+  '3d': ['column', 'pie', 'bar'],
+  'reg': ['line', 'pie', 'spline']
+}
+// 3D Config
+let enable3D = true
+let chart3DOptions = { alpha: 40, beta: 0, depth: 50 }
+
+// Text Config
+let fontSize = { titel: '14px', subTitle: '10px' }
+let title = 'ManPower Cataegory'
+let subTitle = 'Admin, BlueCollar, Eng'
+let seriesTitle = 'Employee Count'
+
+// Colors
+let colorTheme = {
+  colorful: ['#8a2be2', '#9932cc', '#4b0082', '#483d8b', '#6a5acd'],
+  blackWhite: ['#8a8a8a', '#999999', '#4b4b4b', '#484848', '#6a6a6a']
+}
+
+
+
+function App() {
+  return (
+    <div className="chart-3" >
+      <HighChart3DComponent
+        // Data
+        chartData={chartData}
+        // Chart Type
+        chartType={chartType['3d'][2]}
+        innerSize={150}
+        // Configuration for 3D Options
+        chartOptions={{
+          enabled: enable3D,
+          alpha: chart3DOptions.alpha,
+          beta: chart3DOptions.beta
+        }}
+        depth={chart3DOptions.depth}
+        // Colors
+        colorTheme={colorTheme.blackWhite}
+        // Text Config
+        titleSize={fontSize.titel}
+        title={title}
+        subTitleSize={fontSize.subTitle}
+        subTitle={subTitle}
+        seriesTitle={seriesTitle}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/chartComponent.js
@@ -0,0 +1,76 @@
+import React, { useEffect } from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import Highcharts3D from 'highcharts/highcharts-3d';
+import exportingInit from 'highcharts/modules/exporting';
+
+Highcharts3D(Highcharts);
+exportingInit(Highcharts);
+
+
+const HighChart3DComponent = ({
+    chartType, chartOptions, chartData, innerSize, depth, colorTheme, title, titleSize, subTitleSize, subTitle, seriesTitle
+}) => {
+    const options = {
+        credits: {
+            enabled: false
+        },
+        exporting: {
+            buttons: {
+                contextButton: {
+                    menuItems: [
+                        'downloadPNG',
+                        'downloadJPEG',
+                        'downloadPDF',
+                        'downloadSVG',
+                        'separator',
+                        'viewFullscreen'
+                    ]
+                }
+            }
+        },
+
+        xAxis: {
+            gridLineWidth: 0 // Disable grid lines on the x-axis
+        },
+        yAxis: {
+            gridLineWidth: 0 // Disable grid lines on the y-axis
+        },
+        chart: {
+            type: chartType,
+            backgroundColor: 'transparent',
+            options3d: chartOptions
+        },
+        title: {
+            text: title,
+            style: {
+                fontSize: titleSize
+            }
+
+        },
+        subtitle: {
+            text: subTitle,
+            align: 'center', style: {
+                fontSize: subTitleSize
+            }
+        },
+        plotOptions: {
+            pie: {
+                innerSize: innerSize,
+                depth: depth
+
+            }
+        },
+
+        colors: colorTheme,
+        series: [{
+
+            name: seriesTitle,
+            data: chartData
+        }]
+    };
+
+    return <HighchartsReact highcharts={Highcharts} options={options} />;
+};
+
+export default HighChart3DComponent;
